3ce53a0b09748b14b13d42626b11c7477cf9d14d
Praveen56861 <43000640+Praveen56861@users.noreply.github.com>
2018-10-08 19:01:04 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello DevOps: src/main/java/Hello.java";
+      message = "Hello DevOps world";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
